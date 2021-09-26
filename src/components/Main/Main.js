ff0a4a91612d6463fecbf61644ca5c1f6ee15a36
Simanta Barua <simanta.barua@yhaoo.com>
2021-09-26 18:40:57 +0600

    cart

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Member from '../Member/Member';
 
 const Main = () => {
-    const {members,setMembers} = useState([])
+    const [members, setMembers] = useState([])
+    useEffect(() => {
+        fetch('profiles.json')
+            .then(res => res.json())
+            .then(data => setMembers(data)
+            )
+    }, [])
     return (
         <div className="container">
             <div className="row">
                 <div className="col-10 border">
                     <div className="row row-cols-1 row-cols-md-3 m-1 g-4">
-                        <Member />
-                        
+                        {
+                            members.map(member => <Member member={member} key={members.key}/>)
+                        }
                     </div>
                 </div>
-                <div className="col-2">col-4</div>
+                <div className="col-2">
+                    <Cart/>
+                </div>
             </div>
         </div>
 
--- src/components/Member/Member.js
+++ src/components/Member/Member.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-const Member = () => {
+const Member = (props) => {
+    console.log(props.member);
+    const {name,age,picture,salary: salary,address} = props.member||{};
     return (
         <div>
             <div className="col">
                 <div className="card h-100">
-                    <img src="..." className="card-img-top" alt="..." />
+                    <img src={picture} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
+                        <h5 className="card-title">{name}</h5>
+                        <p className="card-text">{address}</p>
+                        <p>Salary: ${salary}</p>
                     </div>
                     <div className="card-footer">
-                        <small className="text-muted">Last updated 3 mins ago</small>
+                        <button className="btn btn-primary"><i class="fas fa-shopping-cart"></i>Add to cart</button>
                     </div>
                 </div>
             </div>
