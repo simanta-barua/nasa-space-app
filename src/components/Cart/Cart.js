d61a28498165005abcc7a341a4f42b54ea55f7c7
Simanta Barua <simanta.barua@yhaoo.com>
2021-09-26 23:34:08 +0600

    update json

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,9 +8,6 @@ const Cart = (props) => {
     const totalReducer = (preValue, curValue) => preValue + curValue.salary;
     const total = cart.reduce(totalReducer, 0).toFixed(2)
 
-
-
-
     return (
         <div className="team-cart">
             <h2 className="text-dark text-center">Team list</h2>
@@ -18,11 +15,13 @@ const Cart = (props) => {
             <h4><span className="text-dark ">Total Cost: $</span> {total} </h4>
             <hr />
             <ul className="list-group">
-                {cart.map(member => <li className="list-group-item  list-item">{member.name}   <i className="fas fa-times-circle"></i></li>)}
+                {cart.map(member => <li className="list-group-item  list-item">{member.name}  key={member.key} <i className="fas fa-times-circle" ></i></li> )
+                }
             </ul>
-
         </div>
+        
     );
+    
 };
 
 export default Cart;
